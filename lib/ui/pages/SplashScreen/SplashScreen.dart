65fa1aee74e42df485217ed3299bea5ccbc6d4e3
Stiven Morelo <IngenieroStiven@outlook.es>
2021-05-10 18:01:29 -0500

    Configured

--- lib/ui/pages/SplashScreen/SplashScreen.dart
+++ lib/ui/pages/SplashScreen/SplashScreen.dart
@@ -1,3 +1,7 @@
+import 'package:amplify/amplifyconfiguration.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_analytics_pinpoint/amplify_analytics_pinpoint.dart';
+import 'package:amplify_flutter/amplify.dart';
 import 'package:flutter/material.dart';
 
 class SplasScreen extends StatefulWidget {
@@ -8,8 +12,33 @@ class SplasScreen extends StatefulWidget {
 }
 
 class _SplasScreenState extends State<SplasScreen> {
+  bool _amplifyConfigured = false;
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  void _configureAmplify() async {
+    AmplifyAuthCognito authCognito = AmplifyAuthCognito();
+    AmplifyAnalyticsPinpoint analyticsPlugin = AmplifyAnalyticsPinpoint();
+    Amplify.addPlugins([authCognito, analyticsPlugin]);
+    try {
+      await Amplify.configure(amplifyconfig);
+      setState(() {
+        _amplifyConfigured = true;
+      });
+    } on AmplifyAlreadyConfiguredException {
+      print('No configure');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text(_amplifyConfigured? 'Configured' : 'Not Configured'),
+      ),
+    );
   }
 }
